65194b7044851117dfed9e01c8a4eb20fe3b6f1f
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-08-09 17:01:25 +0300

    updated styles.

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Username = () => {
-  return <div className="text-sm font-semibold">Mohamed</div>;
+  return <div className="hidden text-sm font-semibold md:block">Mohamed</div>;
 };
 
 export default Username;
