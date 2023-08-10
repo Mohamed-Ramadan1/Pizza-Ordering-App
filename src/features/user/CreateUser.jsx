8520b73816de7041b885e09d21d5cd721e9a2268
Mohamed Ramadan <mohamedramadan11b@gmail.com>
2023-08-10 07:42:53 +0300

    Start working on the globel states using redux toolkit.

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { userActions } from './userSlice';
 import Button from '../../ui/Button';
 
 function CreateUser() {
+  const dispatch = useDispatch();
   const [username, setUsername] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
+    dispatch(userActions.updateName(username));
   }
 
   return (
